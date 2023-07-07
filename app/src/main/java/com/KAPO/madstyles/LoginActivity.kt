761b2bfb1bc50533257537618fa34ea0167f5472
Byeongchan Park <pbc1017@kaist.ac.kr>
2023-07-07 21:17:58 +0900

    add login

--- app/src/main/java/com/KAPO/madstyles/LoginActivity.kt
+++ app/src/main/java/com/KAPO/madstyles/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.KAPO.madstyles
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore.Audio.Media
@@ -25,11 +26,6 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
-    private fun requestPermission() {
-        val permission = Manifest.permission.INTERNET
-        ActivityCompat.requestPermissions(this, arrayOf(permission), 1)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -44,15 +40,7 @@ class LoginActivity : AppCompatActivity() {
             binding.inputId.text.clear()
             binding.inputPw.text.clear()
 
-            val permission = Manifest.permission.INTERNET
-
-            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
-                sendLoginRequest(id, pw)
-            } else {
-                requestPermission()
-            }
-            // Send request to the server
-
+            sendLoginRequest(id, pw)
         }
     }
 
@@ -71,52 +59,17 @@ class LoginActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call, response: okhttp3.Response) {
-                Log.d("Result",response.body!!.string())
+                val result = response.body!!.string().removeSurrounding("\"")
+                Log.d("Result","${result}")
+                if (result == "true") {
+                    intent.putExtra("id",id)
+                    setResult(RESULT_OK, intent)
+                    finish()
+                } else {
+//                    Toast.makeText(this@LoginActivity,"ID/PW가 일치하지 않습니다.",Toast.LENGTH_SHORT).show()
+                }
             }
 
         })
-
-
-        /*
-        val params = HashMap<String,String>()
-        params["id"] = id
-        params["password"] = pw
-        val jsonObject = JSONObject(params as Map<*, *>)
-
-        val requestQueue = Volley.newRequestQueue(this)
-        val stringRequest = object : StringRequest(
-            Request.Method.POST, url,
-            Response.Listener { response ->
-                // TODO: handle your server response here
-                // For instance, you could parse it as JSON and check some field to verify the login
-                val responseObject = JSONObject(response)
-
-//                val loginSuccessful = responseObject.getBoolean("success") // Assuming response has "success" field
-                val loginSuccessful = responseObject.toString()
-                Log.d("chan","${loginSuccessful}")
-//                if (loginSuccessful) {
-//                    // Login successful, navigate to next activity
-//                    Log.d("chan","Login successful")
-//                } else {
-//                    // Login failed, show error message
-//                    Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
-//                }
-            },
-            Response.ErrorListener { error ->
-                // TODO: Handle the error appropriately
-                Log.e("chan", "Login request error: $error")
-            }) {
-            override fun getBodyContentType(): String {
-                return "application/json; charset=utf-8"
-            }
-
-            override fun getBody(): ByteArray {
-                return jsonObject.toString().toByteArray()
-            }
-        }
-
-        // Add the request to the RequestQueue.
-        requestQueue.add(stringRequest)
-        */
     }
 }
--- app/src/main/java/com/KAPO/madstyles/MainActivity.kt
+++ app/src/main/java/com/KAPO/madstyles/MainActivity.kt
@@ -1,11 +1,136 @@
 package com.KAPO.madstyles
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.KAPO.madstyles.FiveFragment
+import com.KAPO.madstyles.FourFragment
+import com.KAPO.madstyles.OneFragment
+import com.KAPO.madstyles.R
+import com.KAPO.madstyles.ThreeFragment
+import com.KAPO.madstyles.TwoFragment
+import com.KAPO.madstyles.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var initTime = 0L
+    private var id: String? = ""
+
+
+    // 뷰 페이저 어댑터
+    class MyFragmentPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        val fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment(), FourFragment(), FiveFragment())
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
+    fun tabSetting(tab: TabLayout.Tab, position: Int) {
+        if (position == 0) {
+            tab.text = "tab1"
+//            tab.setIcon(R.drawable.contacts_icon)
+        } else if (position == 1) {
+            tab.text = "tab2"
+//            tab.setIcon(R.drawable.gallery_icon)
+        } else if (position == 2) {
+            tab.text = "tab3"
+//            tab.setIcon(R.drawable.gallery_icon)
+        } else if (position == 3) {
+            tab.text = "tab4"
+//            tab.setIcon(R.drawable.gallery_icon)
+        } else {
+            tab.text = "tab5"
+//            tab.setIcon(R.drawable.gpt_icon)
+        }
+
+    }
+    private fun sendIdRequest(id: String) {
+        // Assuming your server accepts POST request for login with params "username" and "password"
+        val url = "http://143.248.193.204:4444/requestmain"
+        val okHttpClient= OkHttpClient()
+        val JSONobj= JSONObject()
+        JSONobj.put("id",id)
+        val body= JSONobj.toString().toRequestBody("application/json".toMediaType())
+        val req=okhttp3.Request.Builder().url(url).post(body).build()
+        okHttpClient.newCall(req).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("ERROR",e.message.toString())
+            }
+            override fun onResponse(call: Call, response: okhttp3.Response) {
+                val result = response.body!!.string().removeSurrounding("\"")
+                Log.d("Result","${result}")
+//                if (result == "true") {
+//                    intent.putExtra("id",id)
+//                    setResult(RESULT_OK, intent)
+//                    finish()
+//                } else {
+//                    Toast.makeText(this@LoginActivity,"ID/PW가 일치하지 않습니다.",Toast.LENGTH_SHORT).show()
+//                }
+            }
+
+        })
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //TODO: 자동로그인 여부 확인
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivityForResult(intent,10)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.viewpager.adapter = MyFragmentPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabs,binding.viewpager){
+                tab, position-> tabSetting(tab, position)
+        }.attach()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 10 && resultCode === Activity.RESULT_OK) {
+            id = data?.getStringExtra("id")
+        }
+        id?.let { sendIdRequest(it) }
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+//            val fragment = (binding.viewpager.adapter as MyFragmentPagerAdapter).fragments[binding.viewpager.currentItem]
+//            if (fragment is OneFragment) {
+//                if(fragment.backButtonPressed()==1) return true
+//            }
+//            else if (fragment is TwoFragment) {
+//                if(fragment.backButtonPressed()==1) return true
+//            }
+            if(System.currentTimeMillis() - initTime > 3000){
+                Toast.makeText(this, "종료하려면 한번 더 누르세요!",
+                    Toast.LENGTH_SHORT).show()
+                initTime = System.currentTimeMillis()
+                return true // 키 이벤트 무시
+            }
+            else
+            {
+                ActivityCompat.finishAffinity(this)
+                System.exit(0)
+            }
+        }
+        return super.onKeyDown(keyCode, event) // 키 이벤트 처리
     }
 }

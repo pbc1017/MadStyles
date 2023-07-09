b60eb3f68cbae3d435ee88bf5717d5dd5d33195b
Byeongchan Park <pbc1017@kaist.ac.kr>
2023-07-09 23:16:33 +0900

    modify server request

--- app/src/main/java/com/KAPO/madstyles/ThreeFragment.kt
+++ app/src/main/java/com/KAPO/madstyles/ThreeFragment.kt
@@ -1,5 +1,6 @@
 package com.KAPO.madstyles
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,7 +25,8 @@ class ThreeFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var itemAdapter: ItemAdapter
     val items = mutableListOf<Item>()
-    var json: String=""
+    val buttonMap: MutableMap<String, MutableList<Button>> = mutableMapOf()
+    val filter: MutableMap<String, MutableList<String>> = mutableMapOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,9 +38,32 @@ class ThreeFragment : Fragment() {
         recyclerView.layoutManager = GridLayoutManager(context, 2)
         itemAdapter = ItemAdapter(items)
         recyclerView.adapter = itemAdapter
+        val kindview=arrayOf<LinearLayout> (
+            view.findViewById<LinearLayout>(R.id.kindview1),
+            view.findViewById<LinearLayout>(R.id.kindview2),
+            view.findViewById<LinearLayout>(R.id.kindview3),
+            view.findViewById<LinearLayout>(R.id.kindview4),
+            view.findViewById<LinearLayout>(R.id.kindview5))
 
+        val filterButton = arrayOf<Array<String>>(
+            arrayOf<String>("전체","남자","여자"),
+            arrayOf<String>("전체","상의","바지","아우터","신발","가방","모자"),
+            arrayOf<String>("전체","검정", "흰", "빨강", "파랑", "노랑", "초록"),
+            arrayOf<String>("전체","높음", "중간", "낮음"),
+            arrayOf<String>("인기순","낮은 가격순","높은 가격순"),)
+
+        val groups = arrayOf<String>("성별","종류","색상","가격","정렬")
+        var i = 0
+        for (ll in kindview) {
+            for (btnName in filterButton[i]) {
+                ll.addView(createButton(btnName,groups[i]))
+            }
+            i += 1
+        }
+        filter["정렬"] = mutableListOf("인기순") // 정렬 기본값 설정
         return view
     }
+
     val similarityMap = mapOf(
         "검은색" to "검정",
         "검은" to "검정",
@@ -101,5 +128,79 @@ class ThreeFragment : Fragment() {
 //        {
 //            requestRanking(id,gender)
 //        }
+    }
+
+
+    fun createButton(name: String, group: String): Button {
+        val btn = Button(context)
+        btn.text = name
+        btn.layoutParams =
+            LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        btn.setBackgroundColor(Color.GRAY) // 기본 버튼 색상
+        btn.setOnClickListener {
+            val button = it as Button
+            val currentList = filter[group] ?: mutableListOf()
+            val currentButtonList = buttonMap[group] ?: mutableListOf()
+            if (name == "전체") {
+                if (currentList.contains("전체")) {
+                    // "전체" 버튼이 눌려있을 경우 필터를 비우고 버튼 색상을 초기화합니다.
+                    filter.remove(group)
+                    currentList.clear()
+                    currentButtonList.forEach { it.setBackgroundColor(Color.GRAY) }
+                } else {
+                    // "전체" 버튼이 눌리지 않은 경우 필터를 "전체"로 설정하고 버튼 색상을 바꿉니다.
+                    filter[group] = mutableListOf("전체")
+                    currentButtonList.forEach { it.setBackgroundColor(Color.WHITE) }
+                }
+            } else {
+                if (currentList.contains("전체")) {
+                    // "전체" 버튼이 눌려있으면 다른 버튼을 눌렀을 때 "전체"를 필터에서 제거하고, 해당 버튼을 제외한 다른 버튼들을 필터에 추가합니다.
+                    currentList.remove("전체")
+                    currentButtonList.find { it.text == "전체" }?.setBackgroundColor(Color.GRAY)
+                    for (otherButton in currentButtonList) {
+                        if (otherButton.text != name && !currentList.contains(otherButton.text.toString())) {
+                            currentList.add(otherButton.text.toString())
+                            otherButton.setBackgroundColor(Color.WHITE)
+                        }
+                    }
+                    button.setBackgroundColor(Color.GRAY)
+                } else {
+                    if (currentList.contains(name)) {
+                        // 이미 선택된 버튼을 다시 클릭하면 필터에서 해당 항목을 제거하고 버튼 색상을 초기화합니다.
+                        currentList.remove(name)
+                        button.setBackgroundColor(Color.GRAY)
+                    } else {
+                        // 선택되지 않은 버튼을 클릭하면 필터에 해당 항목을 추가하고 버튼 색상을 바꿉니다.
+                        currentList.add(name)
+                        button.setBackgroundColor(Color.WHITE)
+                    }
+                }
+                // 전체 버튼을 제외한 모든 버튼이 선택되었다면 전체 버튼이 눌려진 것으로 처리합니다.
+                if (currentButtonList.all { it.text == "전체" || it.currentTextColor == Color.WHITE }) {
+                    filter[group] = mutableListOf("전체")
+                    currentButtonList.forEach { it.setBackgroundColor(if(it.text == "전체") { Color.WHITE} else {Color.GRAY })}
+                }
+            }
+            Log.d("FILTER",filter.toString())
+        }
+        val currentButtonList = buttonMap[group] ?: mutableListOf()
+        currentButtonList.add(btn)
+        buttonMap[group] = currentButtonList
+        return btn
+    }
+
+    fun changeButton(newFilter: Map<String, List<String>>) {
+        for ((group, values) in newFilter) {
+            filter[group] = values.toMutableList()
+            // 이제 필터가 변경되었으므로, 버튼의 색상도 업데이트해야 합니다.
+            val currentButtonList = buttonMap[group] ?: mutableListOf()
+            for (button in currentButtonList) {
+                if (values.contains(button.text.toString())) {
+                    button.setBackgroundColor(Color.WHITE)
+                } else {
+                    button.setBackgroundColor(Color.GRAY)
+                }
+            }
+        }
     }
 }
